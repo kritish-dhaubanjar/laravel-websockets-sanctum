99bb82d7d230d1b48c815499aec5eb900ab3da48
kritish-dhaubanjar <kritish.dhaubanjar@gmail.com>
2021-08-10 08:53:41 +0545

    Chat event added.

---
+++ laravel/app/Events/Chat.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class Chat implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $payload = 'Hello World!';
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('App.Models.User.1');
+    }
+
+    /**
+     * Get the event's name to broadcast as.
+     */
+    public function broadcastAs()
+    {
+        return 'new-message-event';
+    }
+}
